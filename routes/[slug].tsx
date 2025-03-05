d6e99a5112c730b6fbe692e54cef59e9646b18a9
Tibor Lancsak <40690847+lancsaktibi@users.noreply.github.com>
2025-03-05 15:08:10 +0000

    added gfm for md formattting

--- routes/[slug].tsx
+++ routes/[slug].tsx
@@ -2,9 +2,11 @@
 // getPost: the post content reader
 // Handlers: performs a task once the route is called 
 // PageProps: an object to store and transfer data across pages
+// CSS, render: markdown formatting from gfm
 import { Post, getPost } from "../utils/posts.ts";
 import { Handlers } from "$fresh/server.ts";
 import { PageProps } from "$fresh/server.ts";
+import { CSS, render } from "@deno/gfm"
 
 // activate handler to call getPost() that reads the post content from the disk
 export const handler: Handlers<Post> = {
@@ -16,8 +18,10 @@ export const handler: Handlers<Post> = {
 };
 
 // render the index page and use post content from PageProps
+// dangerouslySetInnerHTML={{ __html: post.content }}
 export default function PostPage(props: PageProps<Post>) {
   const post = props.data;
+  const markdown = render(post.content);
   return (
     <main class="max-w-screen-md px-4 pt-16 mx-auto">
       <h1 class="text-5xl font-bold">{post.title}</h1>
@@ -28,7 +32,7 @@ export default function PostPage(props: PageProps<Post>) {
           day: "numeric"
         })}
       </time>
-      <div class="mt-8"
+      <div class="mt-8 markdown-body" 
         dangerouslySetInnerHTML={{ __html: post.content }}
         />
     </main>
